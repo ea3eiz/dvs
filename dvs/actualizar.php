c9323a074c490e4512048a7430d93755fd2485ed
Manel <ea3eiz@ea3eiz.com>
2021-05-21 22:21:17 +0200

    1

--- dvs/actualizar.php
+++ dvs/actualizar.php
@@ -1,10 +1,8 @@
 <?php 
 session_start();
-//Actualizar revision
-exec("sudo rm -r Revisiones_VPS");
-exec("git clone http://github.com/ea3eiz/Revisiones_VPS");
-exec("sudo chmod 777 -R Revisiones_VPS");
-exec("sudo chmod 777 -R /var/www/html");
-exec("sudo sh /var/www/html/Revisiones_VPS/actualizar.sh");
-header("Location:/dvs/actualizando_imagen.php");
+//Actualizar
+ 
+exec("cd /var/www/html;git pull");
+
+header("index_dvswitch_buster.php");
 ?>
