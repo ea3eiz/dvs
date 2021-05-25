a94b446f4afc19e3ef647e5c604121d4bca6bf45
Manel <ea3eiz@ea3eiz.com>
2021-05-25 18:07:06 +0200

    1

--- cambia_puerto.php
+++ cambia_puerto.php
@@ -6,6 +6,10 @@ $usuario = strip_tags($_POST['user']);
 exec("sudo sed -i '55c txPort = $puerto' /opt/Analog_Bridge/Analog_Bridge.ini");
 exec("sudo sed -i '56c rxPort = $puerto' /opt/Analog_Bridge/Analog_Bridge.ini");
 
+
+exec("sudo sed -i '5c define("ABINFO","$puerto");' /var/www/html/dvs/include/config.php");
+
+
 //RESTAURAR LOS SERVICIOS
 exec("sudo systemctl restart analog_bridge.service");
 exec("sudo systemctl restart md380-emu.service");
--- include/config.php
+++ include/config.php
@@ -1,5 +1,5 @@
 <?php
-// Report all errors except E_NOTICE
+// Report all errors except E_NOTICE 
 error_reporting(E_ALL & ~E_NOTICE);
 
 define("ABINFO","50362");
