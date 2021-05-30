c60d681795fbfd52ad3320faa3810a44275f49eb
Manel <ea3eiz@ea3eiz.com>
2021-05-30 19:05:17 +0200

    1

--- config/cambia_callsign_plus.php
+++
@@ -1,9 +0,0 @@
-<?php 
-session_start();
-
-$callsign=strtoupper($_POST["callsign"]);
-exec("sudo sed -i '2c Callsign=$callsign' /opt/MMDVM_Bridge/MMDVM_Bridge_PLUS.ini");
-
-header("Location:/dvs/sistema_plus.php");	
-
-?>
