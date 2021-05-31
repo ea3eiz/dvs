18d9bb446e81a2aff7109a1ab8ea9e6bf08107f4
Manel <ea3eiz@ea3eiz.com>
2021-05-31 10:11:06 +0200

    1

--- config/agregar_ysf.php
+++ config/agregar_ysf.php
@@ -7,9 +7,8 @@ $sala="aderdigital.ddns.net:42000";
 # guardamos el valor para luego recuperarlo llamando al script agrgar_ysf.sh
 exec("sudo sed -i '1c $nombre_sala' /home/pi/.local/datos_dvswitch");
 exec("sudo sed -i '2c $sala' /home/pi/.local/datos_dvswitch");
-exec("cd /var/www/html/dvs; sudo sh agregar_ysf.sh");
+exec("cd /var/www/html/dvs/config; sudo sh agregar_ysf.sh");
 
 
 
-/* exec("sudo sed '13a\{ "disp": "$nombre_sala" , "tg": "$sala :42000"},' /opt/Analog_Reflector/arRoot/modes/YSF_node_list.json > YSF_node_list.json");
- */?>
+ ?>
