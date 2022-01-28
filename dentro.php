b352ca91e6ebd07b6512533c01411caaf86b26a6
Manel <ea3eiz@ea3eiz.com>
2022-01-28 10:55:55 +0100

    1

--- dentro.php
+++ dentro.php
@@ -1,7 +1,7 @@
 <?php  	
 session_start();
 $passIng = strip_tags(sha1($_POST['pass']));
-$mipass="7f8e6dcf857891892afadc2b8b0a640a297afaed";
+$mipass="5a9068706389c413b5edf93d2e8b8102caddcc91";
 if ($passIng==$mipass){
 $_SESSION['permisos'] = 1;
 header("Location:index_dvswitch_buster.php");	
