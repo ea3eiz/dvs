31f2f8a88f61ad47ffaa607595056c12a3082f9d
Manel <ea3eiz@ea3eiz.com>
2022-02-02 00:14:08 +0100

    1

--- config/cambia_sala_FCS.php
+++ config/cambia_sala_FCS.php
@@ -11,7 +11,7 @@ exec("sudo systemctl restart analog_bridge.service");
 exec("sudo systemctl restart ircddbgateway.service");
 exec("sudo systemctl restart md380-emu.service");
 exec("sudo systemctl restart mmdvm_bridge.service");
-exec("sudo systemctl restart nxdngateway.service");
+exec("sudo systemctl restart nxdngatewayd.service");
 exec("sudo systemctl restart dmr2ysf.service");
 
 header("Location:/dvs/config/editor_general.php");	
