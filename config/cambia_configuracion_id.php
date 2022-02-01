3011dfecacde3cb15ad06f531a58d04083802009
Manel <ea3eiz@ea3eiz.com>
2022-02-01 19:45:54 +0100

    1

--- config/cambia_configuracion_id.php
+++ config/cambia_configuracion_id.php
@@ -7,6 +7,7 @@ $id7cifras = substr("$id", 0, 7);
 exec("sudo sed -i '3c Id=$id' /opt/MMDVM_Bridge/MMDVM_Bridge_BM.ini");
 exec("sudo sed -i '3c Id=$id' /opt/MMDVM_Bridge/MMDVM_Bridge_PLUS.ini");
 exec("sudo sed -i '3c Id=$id' /opt/MMDVM_Bridge/MMDVM_Bridge_ESPECIAL.ini");
+exec("sudo sed -i '3c Id=$id' /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini");
 exec("sudo sed -i '39c gatewayDmrId = $id7cifras' /opt/Analog_Bridge/Analog_Bridge.ini");
 exec("sudo sed -i '40c repeaterID = $id' /opt/Analog_Bridge/Analog_Bridge.ini");
 
--- config/cambia_configuracion_indicativo.php
+++ config/cambia_configuracion_indicativo.php
@@ -6,6 +6,7 @@ $_SESSION["indicativo"]=$indicativo;
 exec("sudo sed -i '2c Callsign=$indicativo' /opt/MMDVM_Bridge/MMDVM_Bridge_BM.ini");
 exec("sudo sed -i '2c Callsign=$indicativo' /opt/MMDVM_Bridge/MMDVM_Bridge_PLUS.ini");
 exec("sudo sed -i '2c Callsign=$indicativo' /opt/MMDVM_Bridge/MMDVM_Bridge_ESPECIAL.ini");
+exec("sudo sed -i '2c Callsign=$indicativo' /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini");
 
 exec("sudo sed -i '2c gatewayCallsign=$indicativo' /etc/ircddbgateway");
 exec("sudo sed -i '13c repeaterCall1=$indicativo' /etc/ircddbgateway");
