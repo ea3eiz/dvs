58e07944b9525d11c47ce7b54d4f2066e3aa909a
Manel <ea3eiz@ea3eiz.com>
2022-02-05 15:53:51 +0100

    1

--- config/cambia_configuracion_id.php
+++ config/cambia_configuracion_id.php
@@ -11,6 +11,8 @@ exec("sudo sed -i '3c Id=$id' /opt/MMDVM_Bridge/MMDVM_Bridge_FCS.ini");
 exec("sudo sed -i '39c gatewayDmrId = $id7cifras' /opt/Analog_Bridge/Analog_Bridge.ini");
 exec("sudo sed -i '40c repeaterID = $id' /opt/Analog_Bridge/Analog_Bridge.ini");
 
+exec("sudo sed -i '5c Id=$id7cifras' /opt/YSFGateway/YSFGateway.ini");
+
 header("Location:/dvs/config/editor_general.php");	
 
 ?>
