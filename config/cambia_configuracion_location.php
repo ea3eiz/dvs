08af5a21bdcee58432d1610449ceada23ac37b76
Manel <ea3eiz@ea3eiz.com>
2022-02-09 20:44:53 +0100

    1

--- config/cambia_configuracion_location.php
+++ config/cambia_configuracion_location.php
@@ -5,6 +5,7 @@ $location=($_POST["location"]);
 exec("sudo sed -i '14c Location=$location' /opt/MMDVM_Bridge/MMDVM_Bridge_BM.ini");
 exec("sudo sed -i '14c Location=$location' /opt/MMDVM_Bridge/MMDVM_Bridge_PLUS.ini");
 exec("sudo sed -i '14c Location=$location' /opt/MMDVM_Bridge/MMDVM_Bridge_ESPECIAL.ini");
+exec("sudo sed -i '14c Location=$location' /opt/MMDVM_Bridge/MMDVM_Bridge_TGIF.ini");
 
 exec("sudo sed -i '10c description1=$location' /etc/ircddbgateway");
 
